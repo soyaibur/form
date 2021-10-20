1c2e2d6e0fd9758a1f3c2e6138a61efb4d76c78a
soyaibur <ovisoyaiburrahman@gmail.com>
2021-10-20 09:13:15 +0600

    js validation added

--- style.js
+++ style.js
@@ -1,5 +1,25 @@
 const email = document.getElementById("email")
 const password = document.getElementById("password")
 const submit = document.getElementById("submit")
-const error = document.getElementById("error").innerHTML = "I am The Error"
+const error = document.getElementById("error")
+const form = document.getElementsByClassName("form")[0]
+
+submit.addEventListener("click",(e)=>{
+   let messages = []
+   if(email.value === "" || email.value === null){
+       messages.push("Email needed")
+   }
+   if(password.value == "password" || password.value < 6){
+       messages.push("Need a valid password")
+   }
+   if(password.value > 20){
+       messages.push("your ps should be less than 20")
+   }
+   if(messages.length > 0){
+     e.preventDefault()
+     error.innerText = messages.join(", ")
+   }
+})
+
+
 
